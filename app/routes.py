4d82927929b94381c944e80556063ff4bc989d9f
A-res-cd <aariessapin@gmail.com>
2026-04-26 22:15:08 +0800

    changed initial template

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
-from .authentication_db.mysql import register_user, login_user
-from .capstone_db.mongodb import get_all_capstones, insert_capstone, delete_capstone
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
