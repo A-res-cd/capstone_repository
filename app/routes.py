11386dbfebebe12d6187d42578ba7379531c924f
Hailey Joel <heyliimanuel@gmail.com>
2026-05-08 14:54:30 +0800

    copied ares and added a route for add capstone record for admin

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from .variables.variable import get_nav_links, get_role_meta, resolve_title
 
 main = Blueprint("main", __name__)
 
+
 @main.app_context_processor
 def inject_global_template_vars():
     role = session.get("role", "Admin")
@@ -21,35 +22,45 @@ def inject_global_template_vars():
         "page_title": page_title,
     }
 
-## --- Landings & Auth routes ---
+# --- Landings & Auth routes ---
+
 
 @main.route("/")
 def home():
     return render_template("index.html", hide_nav=True)
 
+
 @main.route("/signin")
 def signin():
     return render_template("authentication/signin.html", hide_nav=True, hide_header=True)
 
+
 @main.route('/signup')
 def signup():
     return render_template("authentication/signup.html", hide_nav=True, hide_header=True)
+
 
 @main.route("/logout")
 def logout():
     return render_template("index.html", hide_nav=True, hide_header=True)
 
 
-
-## --- Admin specific routes ---
+# --- Admin specific routes ---
 @main.route("/analytics")
 def admin_analytics():
     return render_template("admin/analytics.html", hide_nav=False)
+
 
 @main.route("/manage_users")
 def admin_manage_users():
     return render_template("admin/manage_users.html", hide_nav=False)
 
+
 @main.route("/requests")
 def admin_requests():
     return render_template("admin/requests.html", hide_nav=False)
+
+
+@main.route("/add_capstone_record")
+def admin_add_capstone_record():
+    return render_template("admin/add_capstone_record.html", hide_nav=False)
