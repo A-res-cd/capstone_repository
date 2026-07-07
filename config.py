5f2637d3efe7434261c943a15560487fa7c456b2
Hailey Joel <heyliimanuel@gmail.com>
2026-07-07 19:06:17 +0800

    Fixed the csrf token problem when changing roles of others using admin, new problem arose: after changing the role of an admin into a user. it still has admin privileges despite not being an admin anymore. should fix that asap

--- config.py
+++ config.py
@@ -3,14 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-
-    # MySQL connection (must be edited to match your MySQL configuration)
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
-    MYSQL_USER = os.getenv("MYSQL_USER")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_DB = os.getenv("MYSQL_DB")
 
     PG_HOST = os.getenv("PG_HOST")
     PG_PORT = int(os.getenv("PG_PORT", "5432"))
@@ -18,10 +13,10 @@ class Config:
     PG_PASSWORD = os.getenv("PG_PASSWORD")
     PG_DB = os.getenv("PG_DB")
 
-    MAIL_SERVER         = os.environ.get("MAIL_SERVER")
-    MAIL_PORT           = int(os.environ.get("MAIL_PORT"))
-    MAIL_USE_TLS        = True
-    MAIL_USERNAME       = os.environ.get("MAIL_USERNAME")   
-    MAIL_PASSWORD       = os.environ.get("MAIL_PASSWORD")   
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT"))
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
     UPLOAD_FOLDER = 'app/static/uploads'
